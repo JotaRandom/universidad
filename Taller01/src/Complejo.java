2f9d07b2526a85d7fdd8648a1e80459954ab7948
Pablo Roberto Francisco Lezaeta Reyes <prflr88@gmail.com>
2014-04-03 21:00:10 -0300

    Taller 01
    Signed-off-by: Pablo Roberto Francisco Lezaeta Reyes <prflr88@gmail.com>

---
+++ Taller01/src/Complejo.java
@@ -0,0 +1,14 @@
+
+public class Complejo {
+	private double re;
+	private double im;
+	@Override
+	public String toString() {
+		return "Complejo [re=" + re + ", im=" + im + "]";
+	}
+	public Complejo(double re, double im) {
+		super();
+		this.re = re;
+		this.im = im;
+	}
+}
---
+++ Taller01/src/ProgramaPrincipal.java
@@ -0,0 +1,12 @@
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		double a, b, c;
+		a = 10.4;
+		b = 11.3;
+		c = a + b;
+		System.out.println(a + "+" + b + "=" + c);
+	}
+
+}
